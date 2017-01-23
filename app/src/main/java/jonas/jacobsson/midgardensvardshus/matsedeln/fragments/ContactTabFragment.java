988c0f6d388c8c44aab6188603a7db24062a81d0
92jonas <jacobsson_jonas@hotmail.com>
2017-01-23 23:32:33 +0100

    Email and edittext fixed. Also keyboard appearance

--- app/src/main/java/jonas/jacobsson/midgardensvardshus/matsedeln/fragments/ContactTabFragment.java
+++ app/src/main/java/jonas/jacobsson/midgardensvardshus/matsedeln/fragments/ContactTabFragment.java
@@ -1,17 +1,17 @@
 package jonas.jacobsson.midgardensvardshus.matsedeln.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import butterknife.OnLongClick;
 import jonas.jacobsson.midgardensvardshus.matsedeln.R;
 
 /**
@@ -20,20 +20,13 @@ import jonas.jacobsson.midgardensvardshus.matsedeln.R;
 
 public class ContactTabFragment extends Fragment {
 
-    @BindView(R.id.textView5)
-    TextView textView5;
-    @BindView(R.id.textView2)
-    TextView textView2;
-    @BindView(R.id.editText)
-    EditText editText;
-    @BindView(R.id.textView3)
-    TextView textView3;
-    @BindView(R.id.editText2)
-    EditText editText2;
-    @BindView(R.id.textView4)
-    TextView textView4;
-    @BindView(R.id.editText3)
-    EditText editText3;
+
+    @BindView(R.id.et_name)
+    EditText etName;
+    @BindView(R.id.et_email)
+    EditText etEmail;
+    @BindView(R.id.et_forslag)
+    EditText etText;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -52,14 +45,18 @@ public class ContactTabFragment extends Fragment {
         super.onDestroyView();
     }
 
-    @OnClick(R.id.button2)
-    void onButton2Click() {
-        //TODO implement
+    @OnClick(R.id.btn_send)
+    void onSendClick() {
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("message/rfc822");
+        i.putExtra(Intent.EXTRA_EMAIL, new String[]{"jacobsson_jonas@hotmail.com"});
+        i.putExtra(Intent.EXTRA_SUBJECT, "APP: Matförslag");
+        i.putExtra(Intent.EXTRA_TEXT, etText.getText().toString());
+        try {
+            startActivity(Intent.createChooser(i, "Send mail..."));
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(getContext(), "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+        }
     }
 
-    @OnLongClick(R.id.button2)
-    boolean onButton2LongClick() {
-        //TODO implement
-        return true;
-    }
 }
